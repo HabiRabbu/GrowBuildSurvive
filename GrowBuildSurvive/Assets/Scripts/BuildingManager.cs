c47af758580eacbe41463d30d693384e399698a7
HabiRabbu <64276030+HabiRabbu@users.noreply.github.com>
2021-10-25 16:26:06 +0100

    V1

---
+++ GrowBuildSurvive/Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+
+public class BuildingManager : MonoBehaviour
+{
+    public static BuildingManager Instance { get; private set; }
+
+    public event EventHandler<OnActiveBuildingTypeChangedEventArgs> OnActiveBuildingTypeChanged;
+
+    public class OnActiveBuildingTypeChangedEventArgs : EventArgs
+    {
+        public BuildingTypeSO activeBuildingType;
+    }
+
+    private BuildingTypeSO activeBuildingType;
+    private BuildingTypeListSO buildingTypeList;
+
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
+    }
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            if (activeBuildingType != null && CanSpawnBuilding(activeBuildingType, UtilsClass.GetMouseWorldPosition()))
+            {
+                if (ResourceManager.Instance.CanAfford(activeBuildingType.constructionResourceCostArray))
+                {
+                    ResourceManager.Instance.SpendResources(activeBuildingType.constructionResourceCostArray);
+                    Instantiate(activeBuildingType.prefab, UtilsClass.GetMouseWorldPosition(), Quaternion.identity);
+                }
+            }
+        }
+    }
+
+
+
+    public void SetActiveBuildingType(BuildingTypeSO buildingType)
+    {
+        activeBuildingType = buildingType;
+        OnActiveBuildingTypeChanged?.Invoke(this, new OnActiveBuildingTypeChangedEventArgs { activeBuildingType = activeBuildingType });
+    }
+
+    public BuildingTypeSO GetActiveBuildingType()
+    {
+        return activeBuildingType;
+    }
+
+    private bool CanSpawnBuilding(BuildingTypeSO buildingType, Vector3 position)
+    {
+        BoxCollider2D boxCollider2D = buildingType.prefab.GetComponent<BoxCollider2D>();
+
+        Collider2D[] collider2DArray = Physics2D.OverlapBoxAll(position + (Vector3)boxCollider2D.offset, boxCollider2D.size, 0);
+
+        bool isAreaClear = collider2DArray.Length == 0;
+        if (!isAreaClear) return false;
+
+        collider2DArray = Physics2D.OverlapCircleAll(position, buildingType.minConstructionRadius);
+
+        foreach (Collider2D collider2D in collider2DArray)
+        {
+            BuildingTypeHolder buildingTypeHolder = collider2D.GetComponent<BuildingTypeHolder>();
+            if (buildingTypeHolder != null)
+            {
+                if (buildingTypeHolder.buildingType == buildingType)
+                {
+                    return false;
+                }
+            }
+        }
+
+        float maxConstructionRadius = 25;
+        collider2DArray = Physics2D.OverlapCircleAll(position, maxConstructionRadius);
+
+        foreach (Collider2D collider2D in collider2DArray)
+        {
+            BuildingTypeHolder buildingTypeHolder = collider2D.GetComponent<BuildingTypeHolder>();
+            if (buildingTypeHolder != null)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ GrowBuildSurvive/Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ResourceManager : MonoBehaviour
+{
+
+    public static ResourceManager Instance { get; private set; }
+
+    public event EventHandler OnResourceAmountChanged;
+
+    private Dictionary<ResourceTypeSO, int> resourceAmountDictionary;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        //Creating a sort of list where each "key" (resourceType of each ResourceTypeSO)
+        //has an int (in this case) assigned to it.
+        //e.g Debug.Log(resourceAmountDictionary["Wood"]) -> Outputs -> "0"
+        resourceAmountDictionary = new Dictionary<ResourceTypeSO, int>();
+
+        ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(typeof(ResourceTypeListSO).Name);
+
+        foreach (ResourceTypeSO resourceType in resourceTypeList.list)
+        {
+            resourceAmountDictionary[resourceType] = 0;
+        }
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(typeof(ResourceTypeListSO).Name);
+            AddResource(resourceTypeList.list[0], 2);
+        }
+    }
+
+    private void TestLogResourceAmountDictionary()
+    {
+        foreach (ResourceTypeSO resourceType in resourceAmountDictionary.Keys)
+        {
+            Debug.Log(resourceType.nameString + ": " + resourceAmountDictionary[resourceType]);
+        }
+    }
+
+    public void AddResource(ResourceTypeSO resourceType, int amount)
+    {
+        resourceAmountDictionary[resourceType] += amount;
+
+        OnResourceAmountChanged?.Invoke(this, EventArgs.Empty);
+
+    }
+
+    public int GetResourceAmount(ResourceTypeSO resourceType)
+    {
+        return resourceAmountDictionary[resourceType];
+    }
+
+    public bool CanAfford(ResourceAmount[] resourceAmountArray)
+    {
+        foreach (ResourceAmount resourceAmount in resourceAmountArray)
+        {
+            if (GetResourceAmount(resourceAmount.resourceType) >= resourceAmount.amount)
+            {
+
+            } else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void SpendResources(ResourceAmount[] resourceAmountArray)
+    {
+        foreach (ResourceAmount resourceAmount in resourceAmountArray)
+        {
+            resourceAmountDictionary[resourceAmount.resourceType] -= resourceAmount.amount;
+        }
+    }
+
+}
